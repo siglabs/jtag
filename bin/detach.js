d66ccf0ae5b30d8c97a168d49864ac3e83074b42
batman <ameet@siglabs.com>
2019-06-04 16:22:27 -0700

    modify detach.js to ignore devices that don't have numerical serial number where the serial numbers have the value less than 9999

--- bin/detach.js
+++ bin/detach.js
@@ -9,10 +9,16 @@ usb.getDeviceList().some((dev, i) => {
         const iSerialNumber = dev.deviceDescriptor.iSerialNumber;
         dev.getStringDescriptor(iSerialNumber, (err, buf) => {
             console.log(i, buf);
+
             const if0 = dev.interfaces[0];
             if (if0.isKernelDriverActive()) {
-                if0.detachKernelDriver();
-                console.log('detached');
+		if(parseInt(buf) != NaN && parseInt(buf) < 9999) {
+                    if0.detachKernelDriver();
+                    console.log('detached');
+		}
+		else {
+		    console.log('Not detached');
+		}
             }
             dev.close();
         });
